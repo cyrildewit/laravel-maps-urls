eef96951ee712ee56dbe5ce55c8ea67c727fd24c
Cyril de Wit <cyrilwit@gmail.com>
2018-05-13 10:59:56 +0200

    feat: finish methods

--- src/MapsUrl.php
+++ src/MapsUrl.php
@@ -1,15 +1,17 @@
 <?php
 
-namespace CyrildeWit\LaravelMapsUrls;
+declare(strict_types=1);
 
 /*
- * This file is part of the Maps URLs package.
+ * This file is part of the Laravel Maps URLs package.
  *
  * (c) Cyril de Wit <github@cyrildewit.nl>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+namespace CyrildeWit\LaravelMapsUrls;
 
 use CyrildeWit\MapsUrls\MapsUrl as OriginalMapsUrl;
 use CyrildeWit\MapsUrls\Actions\SearchAction;
@@ -18,9 +20,13 @@ use CyrildeWit\MapsUrls\Actions\DirectionAction;
 class MapsUrl
 {
     /**
-     * Create a search URL.
+     * Build a search URL with the given query and query place id.
+     *
+     * @param  string  $query
+     * @param  string|null  $queryPlaceId
+     * @return string
      */
-    public function search(string $query, string $queryPlaceId)
+    public function search(string $query, $queryPlaceId = null)
     {
         $search = (new SearchAction())->setQuery($query);
 
@@ -29,9 +35,17 @@ class MapsUrl
             $search->setQueryPlaceId($queryPlaceId);
         }
 
-        $url = (new OriginalMapsUrl($search))->getUrl();
+        return (new OriginalMapsUrl($search))->getUrl();
     }
 
+    /**
+     * Build a search URL with the given coordinates and query place id.
+     *
+     * @param  float  $lat
+     * @param  float  $lng
+     * @param string|null  $queryPlaceId
+     * @return string
+     */
     public function searchCoordinates(float $lat, float $lng, $queryPlaceId = null)
     {
         $search = (new SearchAction())->setCoordinates($lat, $lng);
@@ -41,30 +55,59 @@ class MapsUrl
             $search->setQueryPlaceId($queryPlaceId);
         }
 
-        $url = (new OriginalMapsUrl($search))->getUrl();
+        return (new OriginalMapsUrl($search))->getUrl();
     }
 
     /**
+     * Build a direction URL with the given origin, destination and travelmode.
      *
+     * @param  string  $origin
+     * @param  string  $destination
+     * @param  string  $travelmode
+     * @return string
      */
     public function direction(string $origin, string $destination, string $travelmode)
     {
-        $direction = (new DirectionAction())->setCoordinates($lat, $lng);
+        $direction = (new DirectionAction())
+            ->setOrigin($origin)
+            ->setDestination($destination)
+            ->setTravelmode($travelmode);
 
-        $url = (new OriginalMapsUrl($direction))->getUrl();
-    }
-
-    public function makeSearch()
-    {
-        $url = new OriginalMapsUrl(new SearchAction());
+        return (new OriginalMapsUrl($direction))->getUrl();
     }
 
     /**
+     * Create a new search action instance.
      *
+     * @param  function|null  $search
+     * @return \CyrildeWit\MapsUrls\Actions\SearchAction
      */
-    public function makeDirection()
+    public function makeSearch($search = null)
     {
-        $url = new OriginalMapsUrl(new DirectionAction());
+        if ($search) {
+            $searchAction = $search(new SearchAction());
+
+            return (new OriginalMapsUrl($searchAction))->getUrl();
+        }
+
+        return new SearchAction();
+    }
+
+    /**
+     * Create a new direction action instance.
+     *
+     * @param  function|null  $direction
+     * @return \CyrildeWit\MapsUrls\Actions\DirectionAction|string
+     */
+    public function makeDirection($direction = null)
+    {
+        if ($direction) {
+            $directionAction = $direction(new DirectionAction());
+
+            return (new OriginalMapsUrl($directionAction))->getUrl();
+        }
+
+        return new DirectionAction();
     }
 }
 
